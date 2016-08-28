67032ffc6fda935b695e2215dc90181d04c8bbb3
fangcengceng <xxxx--->
2016-08-28 16:55:57 +0800

    get方法发送登陆信息，会直接在请求头里面显示出来是不安全的。  而且拼接的URL字符串终不能有中文或者空格

--- 8.28/get方法登陆/get方法登陆/ViewController.m
+++ 8.28/get方法登陆/get方法登陆/ViewController.m
@@ -31,13 +31,20 @@
 //    2016-08-27 14:41:19.042 get方法登陆[2336:132352] request = <NSURLRequest: 0x7fcf11f31660> { URL: (null) }
 //    2016-08-27 14:41:19.203 get方法登陆[2336:132882] data = (null)
 //    httpbody = (null)
+    //URL中？后面的查询字符不能有中文或者空格，要进行%转移；
+//方法在Nnsstring的分类中，记住percent 
+    urlName = [urlName stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
     NSURL *url = [NSURL URLWithString:urlName];
     //创建网络请求者
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
     NSLog(@"request = %@",request);
   [[[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
       NSLog(@"data = %@\n  httpbody = %@",data,request.HTTPBody);
-      
+      if(error == nil & data !=nil){
+          NSLog(@"登陆成功");
+      }else {
+          NSLog(@"登陆失败");
+      }
   }] resume];
     
 }
