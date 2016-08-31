7cae9877cf90b9dddb82f4a45a7c1cd199ae57e8
fangcengceng <xxxx--->
2016-08-31 15:01:37 +0800

    工程提交

---
+++ 8.30/音乐下载/音乐下载/downloadManage.h
@@ -0,0 +1,13 @@
+//
+//  downloadManage.h
+//  音乐下载
+//
+//  Created by codygao on 16/8/30.
+//  Copyright © 2016年 HM. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface downloadManage : NSObject
+
+@end
---
+++ 8.30/音乐下载/音乐下载/downloadManage.m
@@ -0,0 +1,13 @@
+//
+//  downloadManage.m
+//  音乐下载
+//
+//  Created by codygao on 16/8/30.
+//  Copyright © 2016年 HM. All rights reserved.
+//
+
+#import "downloadManage.h"
+
+@implementation downloadManage
+
+@end
